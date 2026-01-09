974fa7f830036fd2ad03cfb702254a477ad69b13
Adil Saifi <adil101010@gmail.com>
2026-01-09 17:00:34 +0530

    Added Promo/Coupon Service - Discount coupons with smart validation

--- cart-service/promo-service/src/main/java/com/fooddelivery/promoservice/enums/CouponStatus.java
+++
@@ -1,7 +0,0 @@
-package com.fooddelivery.promoservice.enums;
-
-public enum CouponStatus {
-    ACTIVE,      // Available for use
-    INACTIVE,    // Temporarily disabled
-    EXPIRED      // Past expiry date
-}
